a008c91391218623234a7c8ef6ecb1711d2cd324
MrUltimate777 <bosaweluche@hotmail.com>
2024-03-20 16:34:51 +0000

    Created letter_map() and started letter_count().

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    #print(text)
     print(word_count(text))
+    #print(letter_map())
 
 def get_book_text(path):
     with open(path) as f:
@@ -12,5 +13,16 @@ def word_count(book_text):
     word_list = book_text.split()
     count = len(word_list)
     return count
+
+def letter_map():
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    letter_map = {}
+    for char in alphabet:
+        letter_map[char] = 0
+    return letter_map
+
+def letter_count(text):
+    
+    pass
 
 main()
